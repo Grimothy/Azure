54d6a7fa0c0aabb7ea285185597d72a267cfced5
Charles Coulter <charlesjcoulter@gmail.com>
2022-10-03 11:16:23 -0400

    chamges

--- Storage/new-StorageStructure.ps1
+++ Storage/new-StorageStructure.ps1
@@ -1,30 +1,60 @@
- #-AccessTier Hot, Cool
-     $tier = "Hot"
- #-Kind Storage, StorageV2, BlobStorage, BlockBlobStorage, FileStorage
+##############################################################################################################################################################
+#-AccessTier Hot, Cool
+   $tier = "Hot"
+#-Kind Storage, StorageV2, BlobStorage, BlockBlobStorage, FileStorage
     $kind = "StorageV2"
+#Location of the resources created
     $location = "Eastus"
+#name of the storage account to be created or the existing storage account
     $name = "catussafiles01"
- #-	StorageAccountType Standard_LRS, Standard_ZRS, Standard_GRS, Standard_RAGRS, Premium_LRS, Premium_ZRS, Standard_GZRS, Standard_RAGZRS
+#-StorageAccountType Standard_LRS, Standard_ZRS, Standard_GRS, Standard_RAGRS, Premium_LRS, Premium_ZRS, Standard_GZRS, Standard_RAGZRS
     $StorageType = "Standard_LRS"
-    $resourcegroup = "rg-catus-safiles01"
+#resourcegroup    
+   $resourcegroup = "rg-catus-storageservices01"
+#Sharename csv path
+   $FSListPath = ".\shares.csv"
+
+##Storage Sync Service name
+$StorageSyncServiceName ="CatuslabSync"
  
-New-AzResourceGroup `
-    -Name $resourcegroup `
-    -Location $location `
+##############################################################################################################################################################
+#Create $resourcegroup for storage account
+Write-Host -ForegroundColor Green "Creating resourcegroup" $resourcegroup
+    New-AzResourceGroup `
+      -Force `
+      -Name $resourcegroup `
+      -Location $location `
+      -Verbose `
+#Create Storage Account
+   New-AzStorageAccount `
+      -Name $name `
+      -Location $location `
+      -ResourceGroupName $resourcegroup `
+      -Kind $kind `
+      -StorageAccountType $StorageType `
+      -AccessTier $tier `
+      -Verbose 
+#Import CSV to get pre-defined sharenames
+   $FSList = Import-Csv -Path $FSListPath
 
- New-AzStorageAccount `
-    -Name $name `
-    -Location $location `
-    -ResourceGroupName $resourcegroup `
-    -Kind $kind `
-    -StorageAccountType $StorageType `
-    -AccessTier $tier `
+    foreach ($sharename in $FSList.sharename )
+   {
+      Write-Host -ForegroundColor Magenta "Creating Files share Share-$sharename under storage account $name"
+      New-AzRmStorageShare `
+         -StorageAccount $(Get-AzStorageAccount -ResourceGroupName $resourcegroup -Name $name) `
+         -Name "share-$sharename" `
+         -EnabledProtocol SMB `
+         -QuotaGiB 100 `
+         -Verbose 
+      Write-Host -ForegroundColor Green "share Share-$sharename has been created"
+      Start-Sleep -Seconds 5
+   }
 
 
-    $shareName = "myshare"
-
-New-AzRmStorageShare `
-    -StorageAccount $name `
-    -Name "test1" `
-    -EnabledProtocol SMB `
-    -QuotaGiB 1024 | Out-Null
+#create Storage Sync Service
+write-host -ForegroundColor Green "Createing Storage Sync Service - SS-$name"
+   New-AzStorageSyncService `
+      -Name "SS-$StorageSyncServiceName-01" `
+      -Location $location `
+      -ResourceGroupName $resourcegroup `
+      -Verbose
